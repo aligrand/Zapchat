fe5a92b97dad20726996781aac4d276f54e14683
mostafa <mostafaytaba@gmail.com>
2023-03-24 15:19:54 +0430

    ui landing page

--- src/landing_page.cpp
+++ src/landing_page.cpp
@@ -6,6 +6,11 @@ landing_page::landing_page(QWidget *parent)
     , ui(new Ui::landing_page)
 {
     ui->setupUi(this);
+    QPixmap Pix("C:/Users/MostafaXD/OneDrive/Desktop/2844296.jpg");
+    ui->Backgrand->setPixmap(Pix.scaled(800,600));
+    ui->groupBox->setStyleSheet("QGroupBox {background-color: #ffffff;	border-radius: 5px;} QGroupBox > * {background-color: transparent;}");
+    QPixmap Wel("C:/Users/MostafaXD/OneDrive/Desktop/welcome.jpg");
+    ui->label->setPixmap(Wel.scaled(331,201));
 }
 
 landing_page::~landing_page()
