791418b522e9f61bb7af0281b6691cedc96257d2
aligrand <ali.workjob@outlook.com>
2023-03-24 16:23:01 +0330

    updated

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,14 @@
 #include "landing_page.h"
 
 #include <QApplication>
+#include <fstream>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+
+
     landing_page w;
     w.show();
 
