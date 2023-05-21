ddb35d4967f1b97097b6fc287d57b18f2d733205
aligrand <ali.workjob@outlook.com>
2023-05-21 16:25:44 +0330

    updated

--- lib/server-connection/serverman.cpp
+++ lib/server-connection/serverman.cpp
@@ -1,6 +1,57 @@
 #include "serverman.h"
 
-ServerMan::ServerMan()
+ServerMan::ServerMan(QString username, QString password)
 {
 
 }
+
+ServerMan::~ServerMan()
+{
+
+}
+
+void ServerMan::userNameExistCheck(QString un)
+{
+
+}
+
+void ServerMan::sendDataProc(QString address)
+{
+
+}
+
+void ServerMan::sendDataBaseProc(QString address)
+{
+
+}
+
+void ServerMan::sendCommandProc(QString cmd)
+{
+
+}
+
+void ServerMan::downloadDataProc(QString address)
+{
+
+}
+
+void ServerMan::newMessage()
+{
+
+}
+
+void ServerMan::messageAsDataProc()
+{
+
+}
+
+void ServerMan::messageAsDataBaseProc()
+{
+
+}
+
+void ServerMan::messageAsCommandProc()
+{
+
+}
+
--- lib/server-connection/serverman.h
+++ lib/server-connection/serverman.h
@@ -1,11 +1,53 @@
 #ifndef SERVERMAN_H
 #define SERVERMAN_H
 
+#include <QApplication>
+#include <QString>
+#include <QByteArray>
+#include <QDataStream>
+#include <QTcpSocket>
+#include <QSqlDatabase>
+#include <QSqlQuery>
+#include <QJsonDocument>
+#include <QJsonObject>
 
 class ServerMan
 {
 public:
-    ServerMan();
+    ServerMan(QString username = "", QString password = "");
+    ~ServerMan();
+
+signals:
+    void isUserNameExist(QString un);
+    void userNameExistResult(bool result);
+    void sendData(QString address);
+    void sendDataBase(QString address);
+    void sendCommand(QString cmd);
+    void downloadData(QString address);
+    void notConnected();
+    void databaseUpdated();
+
+private slots:
+    void userNameExistCheck(QString un);
+    void sendDataProc(QString address);
+    void sendDataBaseProc(QString address);
+    void sendCommandProc(QString cmd);
+    void downloadDataProc(QString address);
+    void newMessage();
+    void messageAsDataProc();
+    void messageAsDataBaseProc();
+    void messageAsCommandProc();
+
+signals:
+    void messageAsData();
+    void messageAsDataBase();
+    void messageAsCommand();
+
+private:
+    QTcpSocket *socket;
+    QSqlDatabase *db;
+    QSqlQuery *dbQuery;
+    QJsonDocument *job;
 };
 
 #endif // SERVERMAN_H
